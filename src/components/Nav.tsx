56439041ec902bccc4c847deb0ff3687b36ac65d
limiu331 <845217637@qq.com>
2020-10-27 22:30:45 +0800

    使用 svg-sprite-loader 搞定icon

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,9 @@
 import styled from "styled-components";
 import React from "react";
 import {Link} from "react-router-dom";
-
+require ('icon/label.svg')
+require ('icon/charge.svg')
+require('icon/statistics.svg')
 const NavWrapper = styled.nav`
 box-shadow: 0 0 3px rgba(0,0,0,0.25);
   ul {
@@ -9,8 +11,15 @@ box-shadow: 0 0 3px rgba(0,0,0,0.25);
     li{
     width: 33.333%;
     text-align: center;
-    padding: 16px;
+    padding: 4px 0;
     line-height: 24px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .icon{
+      width: 24px;
+      height: 24px;
+      }
     }
   }
 `
@@ -19,14 +28,23 @@ const Nav = ()=> {
         <NavWrapper>
             <ul>
                 <li>
+                    <svg className={"icon"}>
+                        <use xlinkHref="#label"/>
+                    </svg>
                     <Link to="/tags">
                         标签页
                     </Link>
                 </li>
                 <li>
+                    <svg className={"icon"}>
+                        <use xlinkHref="#charge"/>
+                    </svg>
                     <Link to="/money">记账页</Link>
                 </li>
                 <li>
+                    <svg className={"icon"}>
+                        <use xlinkHref="#statistics"/>
+                    </svg>
                     <Link to="/statistics">统计页</Link>
                 </li>
             </ul>

c81814607ac7d21c4e0135e30ee4ec17d9b23469
limiu331 <845217637@qq.com>
2020-10-31 12:48:53 +0800

    edit global font color

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,43 @@
 // @ts-ignore
 import React from 'react';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect
 } from "react-router-dom";
 import Tags from "./pages/Tags";
 import Money from "./pages/Money";
 import Statistics from "./pages/Statistics";
 import NotFound from "./pages/NotFound";
+import styled from "styled-components";
+
+const AppWrapper = styled.div`
+color: #333;
+`
+
 function App() {
     return (
-      <Router>
-          <Switch>
-            <Route path="/tags">
-              <Tags />
-            </Route>
-            <Route path="/money">
-              <Money />
-            </Route>
-            <Route path="/statistics">
-              <Statistics />
-            </Route>
-            <Redirect exact from={"/"} to={"tags"}/>
-            <Route path="*">
-              <NotFound/>
-            </Route>
-          </Switch>
-      </Router>
-  );
+        <AppWrapper>
+            <Router>
+                <Switch>
+                    <Route path="/tags">
+                        <Tags/>
+                    </Route>
+                    <Route path="/money">
+                        <Money/>
+                    </Route>
+                    <Route path="/statistics">
+                        <Statistics/>
+                    </Route>
+                    <Redirect exact from={"/"} to={"tags"}/>
+                    <Route path="*">
+                        <NotFound/>
+                    </Route>
+                </Switch>
+            </Router>
+        </AppWrapper>
+    );
 }
+
 export default App;

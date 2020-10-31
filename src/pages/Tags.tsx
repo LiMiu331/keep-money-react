4d7875615095ea6cbe6a8b36e715f66b82132a65
limiu331 <845217637@qq.com>
2020-10-31 13:06:59 +0800

    add noteSection UI for Tags Component

--- src/pages/Tags.tsx
+++ src/pages/Tags.tsx
@@ -27,7 +27,24 @@ const TagSection = styled.section`
  }
 `
 const NotesSection = styled.section`
-
+background: #f5f5f5;
+padding: 0px 16px;
+font-size: 14px;
+  label{
+    display: flex;
+    align-items: center;
+    span{
+      margin-right: 16px;
+      white-space: nowrap;
+    }
+    input{
+      display: block;
+      width: 100%;
+      height: 72px;
+      background: none;
+      border: none;
+    }
+  }
 `
 const CategorySection = styled.section`
 
@@ -51,7 +68,7 @@ function Tags() {
             <NotesSection>
                 <label>
                     <span>备注</span>
-                    <input type="text"/>
+                    <input type="text" placeholder={'请输入备注'}/>
                 </label>
             </NotesSection>
             <CategorySection>

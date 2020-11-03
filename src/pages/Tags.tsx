58abf2152ba17e3db445566a778e096b1f18ec89
limiu331 <845217637@qq.com>
2020-11-03 18:26:17 +0800

    TagsSection 单独封装成函数组件

--- src/pages/Tags.tsx
+++ src/pages/Tags.tsx
@@ -1,131 +1,11 @@
 import Layout from "../components/Layout";
 import React from "react";
 import styled from "styled-components";
+import TagSection from "./Tags/TagSection";
+import NotesSection from "./Tags/NotesSection";
+import CategorySection from "./Tags/CategorySection";
+import NumberPadSection from "./Tags/NumberPadSection";
 
-const TagSection = styled.section`
- flex-grow: 1;
- display: flex;
- flex-direction: column;
- justify-content: flex-end;
- background: #ffffff;
- align-items: flex-start;
- padding: 12px 16px;
- ol{
-  margin: 0 -12px;
-  li{
-    background: rgb(253, 154, 22);
-    border-radius: 18px;
-    display: inline-block;
-    padding: 3px 18px;
-    font-size: 14px;
-    margin: 8px 12px;
-    color: white;
-  }
- }
- button{
- background: none;
- border: none;
- border-bottom: 1px solid #333;
- padding: 2px 4px;
- color: #666;
- margin-top: 8px;
- }
-`
-const NotesSection = styled.section`
-background: #f5f5f5;
-padding: 0px 16px;
-font-size: 14px;
-  label{
-    display: flex;
-    align-items: center;
-    span{
-      margin-right: 16px;
-      white-space: nowrap;
-    }
-    input{
-      display: block;
-      width: 100%;
-      height: 72px;
-      background: none;
-      border: none;
-    }
-  }
-`
-const CategorySection = styled.section`
-font-size: 24px;
-  ul{
-  display: flex;
-  background: white;
-  color: rgb(253, 154, 22);
-  border: 2px solid;
-    li{
-    width: 50%;
-    text-align: center;
-    padding: 16px 0;
-      &.selected{
-        background: rgb(253, 154, 22);
-        color: white;
-      }
-    }
-  }
-`
-const NumberPadSection = styled.section`
-  display: flex;
-  flex-direction: column;
-  .output{
-    background: white;
-    font-size: 36px;
-    line-height: 72px;
-    text-align: right;
-    padding: 0 16px;
-    box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25);
-  }
-  .pad{
-    button{
-    font-size: 18px;
-    float: left;
-    width: 25%;
-    height: 64px;
-    border: none;
-    &.ok{
-      height: 128px;
-      float: right;
-    }
-    &.zero{
-      width: 50%;
-    }
-    &:nth-child(1){
-      background: #f2f2f2;
-    }
-    &:nth-child(2),
-    &:nth-child(5){
-      background: #e0e0e0;
-    }
-    &:nth-child(3),
-    &:nth-child(6),
-    &:nth-child(9){
-      background: #d3d3d3;
-    }
-    &:nth-child(4),
-    &:nth-child(7),
-    &:nth-child(10){
-      background: #c1c1c1;
-    }
-    &:nth-child(8),
-    &:nth-child(11),
-    &:nth-child(13){
-      background: #b8b8b8;
-    }
-    &:nth-child(12){
-      background: #9a9a9a;
-     
-    }
-    &:nth-child(14){
-      background: #a9a9a9;
-    }
-    }
-  }
-`
 const MyLayout = styled(Layout)`
   display: flex;
   flex-direction: column;
@@ -134,14 +14,7 @@ const MyLayout = styled(Layout)`
 function Tags() {
     return (
         <MyLayout>
-            <TagSection className={'tags'}>
-                <ol>
-                    <li>衣</li>
-                    <li>食</li>
-                    <li>住</li>
-                    <li>行</li>
-                </ol>
-                <button>新增标签</button>
+            <TagSection>
             </TagSection>
             <NotesSection>
                 <label>

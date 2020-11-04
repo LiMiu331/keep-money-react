a0c862fbf10d7d08a23476c8136db4cd68000cc3
limiu331 <845217637@qq.com>
2020-11-04 23:24:04 +0800

    finish Tags ui

--- src/pages/Tags.tsx
+++ src/pages/Tags.tsx
@@ -1,10 +1,46 @@
 import Layout from "../components/Layout";
-import React from "react";
+import React, {useState} from "react";
+import styled from "styled-components";
+import Icon from "../components/icon";
 
+const TagList = styled.ol`
+  font-size: 16px;
+  li{
+    border-bottom: 1px solid #d5d5d5;
+    line-height: 20px;
+    padding: 12px 16px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`
+const Button = styled.button`
+  font-size: 18px;
+  border: none;
+  padding: 8px 12px;
+  border-radius: 4px;
+  background:rgba(253, 154, 22) ;
+  color: white;
+  margin-top: 50px;
+`
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 function Tags() {
+    const [tags, setTags] = useState<string[]>(['衣','食','住','行'])
     return (
         <Layout>
-            <h2>标签页</h2>
+            <TagList>
+            {tags.map(tag=> <li key={tag}>
+                <span className={'oneLine'}>{tag}</span>
+                <Icon name={'right'}/>
+            </li>)}
+            </TagList>
+            <Center>
+                <Button>新增标签</Button>
+            </Center>
         </Layout>
     )
 }

df91dc6e5ce1a4cbc857ed66098fe9ca45b64881
limiu331 <845217637@qq.com>
2020-11-05 17:43:57 +0800

    创建Tag Edit 组件 ，并且使用 router exact 精确匹配

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import Money from "./pages/Money";
 import Statistics from "./pages/Statistics";
 import NotFound from "./pages/NotFound";
 import styled from "styled-components";
+import TagEdit from "./pages/Tags/TagEdit";
 
 const AppWrapper = styled.div`
 color: #333;
@@ -21,13 +22,16 @@ function App() {
         <AppWrapper>
             <Router>
                 <Switch>
-                    <Route path="/tags">
+                    <Route path="/tags" exact>
                         <Tags/>
                     </Route>
-                    <Route path="/money">
+                    <Route path="/tags/:tag" exact>
+                        <TagEdit/>
+                    </Route>
+                    <Route path="/money" exact>
                         <Money/>
                     </Route>
-                    <Route path="/statistics">
+                    <Route path="/statistics" exact>
                         <Statistics/>
                     </Route>
                     <Redirect exact from={"/"} to={"/money"}/>

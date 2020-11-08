108399dcf3f1ab1389031f35f85b9394af4ab96e
limiu331 <845217637@qq.com>
2020-11-09 00:09:06 +0800

    删除功能

--- src/pages/Tags/TagEdit.tsx
+++ src/pages/Tags/TagEdit.tsx
@@ -25,29 +25,35 @@ const Center = styled.div`
   align-items: center;
 `
 const TagEdit: React.FC = ()=> {
-    const {findTag, updateTag} = useTags()
+    const {findTag, updateTag, deleteTag} = useTags()
     let { id } = useParams();
     const tag = findTag(parseInt(id))
-    return(
-        <Layout>
-            <Topbar>
-                <Icon name={"left"}/>
-                <span>编辑标签</span>
-                <Icon/>
-            </Topbar>
-            <InputWrapper>
-                <Input
-                    label={'标签名'}
-                    type={'text'}
-                    placeholder={'标签名'}
-                    value={tag.name}
-                    onChange={(e)=> {updateTag(tag.id, {name: e.target.value})}}
-                />
-            </InputWrapper>
-            <Center>
-                <Button>删除标签</Button>
-            </Center>
-        </Layout>
-    )
+    if(tag){
+        return(
+            <Layout>
+                <Topbar>
+                    <Icon name={"left"}/>
+                    <span>编辑标签</span>
+                    <Icon/>
+                </Topbar>
+                <InputWrapper>
+                    <Input
+                        label={'标签名'}
+                        type={'text'}
+                        placeholder={'标签名'}
+                        value={tag.name}
+                        onChange={(e)=> {updateTag(tag.id, {name: e.target.value})}}
+                    />
+                </InputWrapper>
+                <Center>
+                    <Button onClick={()=> deleteTag(tag.id)}>删除标签</Button>
+                </Center>
+            </Layout>
+        )
+    }else {
+        return (
+            <div>tag 不存在</div>
+        )
+    }
 }
 export  default TagEdit;

30b8eda830ba1026a47951b77c83c5921bb36dd0
limiu331 <845217637@qq.com>
2020-11-08 23:29:56 +0800

    使用新的 input 组件

--- src/pages/Money/NotesSection.tsx
+++ src/pages/Money/NotesSection.tsx
@@ -1,25 +1,11 @@
 import styled from "styled-components";
-import React from "react";
+import React, {ChangeEventHandler} from "react";
+import Input from "../../components/Input";
 
 const NotesSectionUI = styled.section`
 background: #f5f5f5;
 padding: 0 16px;
 font-size: 14px;
-  label{
-    display: flex;
-    align-items: center;
-    span{
-      margin-right: 16px;
-      white-space: nowrap;
-    }
-    input{
-      display: block;
-      width: 100%;
-      height: 72px;
-      background: none;
-      border: none;
-    }
-  }
 `
 type Props = {
     value: string,
@@ -29,13 +15,10 @@ const NotesSection:React.FC<Props> = (props) => {
     const note = props.value
     return(
         <NotesSectionUI>
-        <label>
-            <span>备注</span>
-            <input type="text" placeholder={'请输入备注'}
+            <Input label={'备注'}
                    value={note}
-                   onChange={(e) => props.onChange(e.target.value)}
-            />
-        </label>
+                   type="text"
+                   onChange={(e)=> props.onChange(e.target.value)}/>
         </NotesSectionUI>
     )
 }
